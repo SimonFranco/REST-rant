0f5cd45579a3a5dda637b4fe4549da6b649bd4ba
Simon Franco <simonfranco0@gmail.com>
2022-09-12 20:29:29 -0700

    checked routes

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ require('dotenv').config()
 let express = require('express');
 let app = express();
 
+app.use('/places', require('./controllers/places'))
+
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
 app.get('*',function (req,res){
-    res.send(<h1>404 Page</h1>)
+    res.status(404).send('<h1>404 Page</h1>')
 })
 app.listen(process.env.PORT)
