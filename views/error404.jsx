425e4a39274ff32041ff8fcf2604019b0cd7db0b
Simon Franco <simonfranco0@gmail.com>
2022-09-17 17:01:39 -0700

    update bootstrap index

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,7 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+              <img src='/images/Error.avif' alt= 'error' width='20%'/>
           </main>
       </Def>
     )
