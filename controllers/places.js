4c18e68ffd5307bbedfe387db57ebe3a673cfb6d
Simon Franco <simonfranco0@gmail.com>
2022-09-19 04:14:06 -0700

    Form data is getting through

--- controllers/places.js
+++ controllers/places.js
@@ -1,26 +1,30 @@
 const router = require('express').Router()
 
 //get the route that will eventually show the list of places
-router.get('/', (req, res) => {
-	let places = [{
-  name: 'H-Thai-ML',
-  city: 'Seattle',
-  state: 'WA',
-  cuisines: 'Thai, Pan-Asian',
-  pic: 'http://placekitten.com/250/250'
-}, {
-  name: 'Coding Cat Cafe',
-  city: 'Phoenix',
-  state: 'AZ',
-  cuisines: 'Coffee, Bakery',
-  pic: 'http://placekitten.com/250/250'
-}]
-
-    res.render('places/index', { places })
-})
-
 router.get('/new', (req, res) => {
   res.render('places/new')
+})
+router.get('/', (req, res) => {
+  let places = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: 'http://placekitten.com/250/250'
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: 'http://placekitten.com/250/250'
+  }]
+  
+  res.render('places/index', { places })
+})
+
+router.post('/', (req, res) => {
+  console.log(req.body)
+  res.send('Post /places')
 })
 
 
